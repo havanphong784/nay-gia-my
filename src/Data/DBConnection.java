0003a79be70967de809d5d76419d3859d32c7eea
Phongha <phongha@gmail.com>
2026-01-15 09:02:04 +0700

    db

--- src/Data/DBConnection.java
+++ src/Data/DBConnection.java
@@ -5,9 +5,13 @@ import java.sql.DriverManager;
 
 public class DBConnection {
 
-    private static final String URL = "jdbc:sqlserver://XW:1433;databaseName=QL_Kho_Vat_Lieu;encrypt=true;trustServerCertificate=true";
+//    private static final String URL = "jdbc:sqlserver://XW:1433;databaseName=QL_Kho_Vat_Lieu;encrypt=true;trustServerCertificate=true";
+//    private static final String USER = "sa";
+//    private static final String PASSWORD = "123456789";
+    private static final String URL =
+        "jdbc:sqlserver://localhost:1433;databaseName=QL_KHO_VLXD;encrypt=false";
     private static final String USER = "sa";
-    private static final String PASSWORD = "123456789";
+    private static final String PASSWORD = "123456";
     public static Connection getConnection() {
         Connection conn = null;
         try {
